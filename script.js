c56feb8d3393de6812cd067ee0f40aeb3c3f93aa
Bruna Nadoroski <bnadoroski@gmail.com>
2025-12-05 15:36:40 -0300

    pt-eng languages

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ document.addEventListener('click', function (event) {
         document.getElementById('mobile-menu').classList.remove('mobile-menu-show');
     }
 });
+
+const translation = await fetch('lang.json').then(res => res.json());
+const userLang = navigator.language.startsWith('pt') ? 'pt' : 'en';
+setActiveLang(userLang);
+
+document.querySelectorAll('.btn-lang').forEach(button => {
+    button.addEventListener('click', function () {
+        const selectedLang = this.classList.contains('btn-pt') ? 'pt' : 'en';
+        setActiveLang(selectedLang);
+    });
+})
+
+function setActiveLang(lang) {
+    document.querySelectorAll('[data-lang]').forEach(element => {
+        const key = element.getAttribute('data-lang');
+        element.textContent = translation[lang][key];
+    });
+
+    document.querySelectorAll('.btn-lang').forEach(btn => {
+        btn.classList.remove('active');
+    });
+
+    document.querySelector(`.btn-lang.btn-${lang}`).classList.add('active');
+}
